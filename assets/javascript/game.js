3d4d95ee997819cb56fefcc024e85161a67e08b6
rhumel <rhumel@hotmail.com>
2018-06-21 22:59:06 -0400

    addition code to track w/l

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var lowGoal = 19;
 var highGoal= 102;
 var lowFlw = 1;
 var highFlw =12;
+var counter = 0;
 var wins = 0;
 var loss = 0;
 var goalNumber = 0;
@@ -17,6 +18,7 @@ aqua = 0
 
 
 function reset() {
+    var counter = 0;
     var goalNumber = 0;
     var total = 0;
     var flower = [
@@ -41,55 +43,79 @@ for (var i = 0; i < 4; i++) {
     console.log(flower[i]);
 }
 
+console.log(total);
 $("#pink").on("click", function() {
+        
+     if (total < goalNumber) {
+        console.log(flower[0],total);
+        total += flower[0];
+        $("#total").text(total); 
+     }      
     if (total===goalNumber) {
         win();
-    }else if (total > goalNumber) {
+    } 
+    if (total > goalNumber) {
         lose();
-    }else {
-        counter += pink;
-        $("total").text(counter);
-    }
-    
-});
-
-$("#orange").on("click", function(){
-    if (total===goalNumber) {
-        win();
-    }else if (total > goalNumber) {
-        lose();
-    }else {
-        counter += orange;
-        $("total").text(counter);
-    }
-    
+    }   
 });
 
 
-$("#purple").on("click", function(){
+$("#orange").on("click", function() {
+    
+    
+     if (total < goalNumber) {
+        console.log(flower[1],total);
+        total += flower[1];
+        $("#total").text(total); 
+     }      
     if (total===goalNumber) {
         win();
-    }else if (total > goalNumber) {
+    } 
+    if (total > goalNumber) {
         lose();
-    }else {
-        counter += purple;
-        $("total").text(counter);
-    }
-    
+    }   
 });
 
+$("#purple").on("click", function() {
+    console.log("clicked")
+    
+     if (total < goalNumber) {
+        console.log(flower[2],total);
+        total += flower[2];
+        $("#total").text(total); 
+     }      
+     if (total===goalNumber) {
+         win();
+     } 
+     if (total > goalNumber) {
+         lose();
+     }   
+});
 
-$("#aqua").on("click", function(){
+$("#aqua").on("click", function() {
+    console.log("clicked")
+    
+     if (total < goalNumber) {
+        console.log(flower[3],total);
+        total += flower[3];
+        $("#total").text(total); 
+     }      
     if (total===goalNumber) {
-        win();
-    }else if (total > goalNumber) {
-        lose();
-    }else {
-        counter += aqua;
-        $("total").text(counter);
-    }
     
+        win();
+    } 
+    if (total > goalNumber) {
+        lose();
+    }   
 });
+
+    
+    // if (total===goalNumber) {
+    //     win();
+    // } 
+    // if (total > goalNumber) {
+    //     lose();
+    // }
 
 
 
@@ -99,12 +125,17 @@ function randomNumber(max,min) {
 }
 
 function win() {
-    win++;
+  
+    wins++;
+    console.log(wins);
+    $("#wins").text(wins);
     $("#win-loss").text("You Won!!");
 }
 
 function lose() {
     loss++;
+    console.log(loss);
+    $("#losses").text(loss);
     $("#win-loss").text("Try Again!!");
 }
 
